86e20fd3adc6d5e6bd611354da48863906c3b881
Chris Marshall <cmarshall10450@gmail.com>
2017-10-23 15:26:44 +0100

    Added routes for get and post to create a new eatery.

--- src/controllers/eatery.controller.rb
+++ src/controllers/eatery.controller.rb
@@ -11,3 +11,14 @@ get '/eateries/:id' do
 	@eatery = Eatery.find(params[:id])
 	erb(:'eateries/single')
 end
+
+get '/deals/new' do
+	erb(:'burgers/new')
+end
+
+post '/burgers' do
+	burger = Burger.new(params)
+	burger.save
+
+	redirect to '/burgers'
+end
